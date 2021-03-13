a6669d6b796c75598e5ca4d4075bb7afabda86ab
Alex Kaszynski <akascap@gmail.com>
2021-03-12 21:20:36 -0700

    use alex

--- run_image.sh
+++ run_image.sh
@@ -1,2 +1,2 @@
 source IMAGE_NAME
-docker run -it --rm -v /home/alex/python:/mnt/python -v /tmp:/mnt/tmp -p 8888:8888 --gpus all $IMAGE
+docker run -it --rm -v /home/$USER/python:/mnt/python -v /tmp:/mnt/tmp -p 8888:8888 --gpus all $IMAGE
